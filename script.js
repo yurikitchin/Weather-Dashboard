608673513bafce1ba246feb65ca6dc84c83ec02e
Yuri Kitchin <ykitchin@gmail.com>
2021-04-21 11:03:34 +0800

    populate mainDisplay, add new dates toforecast

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var  setLocation = ""
 var latitude = "";
 var longitude = "";
-var dateDisplay = moment(new Date()).format("DD/MM/YYYY")
-document.getElementById("date").innerHTML = dateDisplay
+var dateDisplay = moment(new Date()).format("DD/MM/YYYY");
+document.getElementById("date").innerHTML = dateDisplay;
+var changeDate = new moment().add(1, "day")
+console.log(changeDate.format("DD/MM/YYYY"));
 console.log(dateDisplay);
 var cityName = ""
 var date = document.getElementById('date')
@@ -32,39 +34,59 @@ navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 //q=city is the API syntax for searching a city set q=city as setLocation in search bar
 function fetchFunction() {
 //first fetch function, cycles througuh the forcast data, mataches the date and time to then dt_text then populates the mainDisplay
-    fetch('https://api.openweathermap.org/data/2.5/forecast?'+setLocation+'&units=metric&appid=642a6ac8e3b17623bb06ef71f7b34ae8')
+    fetch('https://api.openweathermap.org/data/2.5/weather?'+setLocation+'&units=metric&appid=642a6ac8e3b17623bb06ef71f7b34ae8')
     .then(function (response){
         return response.json();})
     .then(function (data) {
-        // console.log(data)
+        console.log(data)
         // console.log(data.list[0])
         // console.log(data.list[0].dt_txt)
         // console.log(data.list[0].main.temp)
         fiveDayArray = data.list
-        document.getElementById("city").innerHTML = data.city.name+" ";
-        for (let i = 0; i < fiveDayArray.length; i++) {
-            forecastData = fiveDayArray[i];
-            var ArraydateTime = forecastData.dt_txt;
-            // console.log(ArraydateTime)
-            // console.log(date24hourtime)
-            if (ArraydateTime === date24hourtime) {
-                console.log(forecastData)
-                id="mainTemp"
-                id="mainWind"
-                id="mainHumidity"
-                id="mainUV"
+
+        document.getElementById("city").innerHTML = data.name+" ";
+        //             id="mainTemp"
+        document.getElementById("mainTemp").innerHTML = data.main.temp
+        //             id="mainWind"
+        document.getElementById("mainWind").innerHTML = data.wind.speed
+        //             id="mainHumidity"
+        document.getElementById("mainHumidity").innerHTML = data.main.humidity
+
+        //uv not located in the ist called by this API, run another fetch function to populate the UV indiex
+        return fetch('https://api.openweathermap.org/data/2.5/onecall?'+setLocation+'&exclude=minutely,hourly&appid=642a6ac8e3b17623bb06ef71f7b34ae8&units=metric');})
+        .then(function (response){
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
             }
-            
-            
-        }
-    })
-    .catch (error => console.log(error))}
-    
-    //create variable name, then add city name and date to displayMain box
+        })
+        .then (function (data) {
+            console.log(data)
+            document.getElementById("mainUV").innerHTML = data.current.uvi
+        })
+        
+    .catch (error => console.log(error))};
+        
 
 
 function forecastFunction() {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?'+setLocation+'&exclude=minutely,hourly&appid=642a6ac8e3b17623bb06ef71f7b34ae8&units=metric')
-    .then(response => response.json())
-    .then(data => console.log(data));
+//     //'https://api.openweathermap.org/data/2.5/forecast?'+setLocation+'&units=metric&appid=642a6ac8e3b17623bb06ef71f7b34ae8'
+    fetch('https://api.openweathermap.org/data/2.5/onecall?'+setLocation+'&exclude=current,minutely,hourly&appid=642a6ac8e3b17623bb06ef71f7b34ae8&units=metric')
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        document.querySelector("#forecast1 .forecastDate").innerHTML = new moment().add(1, "day").format("DD/MM/YYYY")
+    })
+//     for (let i = 0; i < fiveDayArray.length; i++) {
+//         forecastData = fiveDayArray[i];
+//         var ArraydateTime = forecastData.dt_txt;
+//         console.log(ArraydateTime)
+//         console.log(date24hourtime)
+//         if (ArraydateTime === date24hourtime) {
+//             console.log(forecastData)
+
 }
+forecastFunction()
